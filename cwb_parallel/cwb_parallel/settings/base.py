b18600625a52db70db0c5ef234d5ac6ea6022e94
dachenlian <cclemon.lian@gmail.com>
2019-04-07 03:32:45 +0800

    updated STATIC_URL; static files work now

--- cwb_parallel/cwb_parallel/settings/base.py
+++ cwb_parallel/cwb_parallel/settings/base.py
@@ -121,12 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-WHITENOISE_STATIC_PREFIX = 'chivar'
+FORCE_SCRIPT_NAME = '/chivar'
+STATIC_URL =  FORCE_SCRIPT_NAME + '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- cwb_parallel/cwb_parallel/wsgi.py
+++ cwb_parallel/cwb_parallel/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from django.conf import settings
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cwb_parallel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cwb_parallel.settings.local')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=settings.STATIC_ROOT)
