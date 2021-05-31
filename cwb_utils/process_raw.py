8c7a96c7f1546f2534b339a85568b455e0f6550b
dachenlian <cclemon.lian@gmail.com>
2021-05-31 12:10:45 +0800

    select specific tasks

--- cwb_utils/process_raw.py
+++ cwb_utils/process_raw.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser, Namespace
 import json
 from pathlib import Path
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, Optional
 
 from bs4 import BeautifulSoup as bs
 from ckip_transformers.nlp import CkipWordSegmenter, CkipPosTagger, CkipNerChunker
@@ -12,14 +12,27 @@ import opencc
 def parse_args() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument(
-        'input_path',
-        help='Text to be processed.',
-        type=Path,
-    )
-    parser.add_argument(
         'output_path',
         help='Path to processed files',
         type=Path
+    )
+    parser.add_argument(
+        '--input_path',
+        help='Text to be processed.',
+        type=Path,
+        default=None
+    )
+    parser.add_argument(
+        '--tasks',
+        help='Select tasks to run in pipeline',
+        choices=['ws', 'pos', 'ner'],
+        nargs='+'
+    )
+    parser.add_argument(
+        '--tokenized_file',
+        help='Load already tokenized file for downstream task (POS tagging)',
+        type=Path,
+        default=None
     )
     parser.add_argument(
         '--to_traditional',
@@ -27,16 +40,28 @@ def parse_args() -> Namespace:
         action='store_true'
     )
     parser.add_argument(
-        '--model_level',
-        help='Choose which model to use for inference (1: albert-tiny, 2: albert-base, 3: bert-base)',
+        '--tokenizer_model_level',
+        help='Choose which model to use for tokenization (1: albert-tiny, 2: albert-base, 3: bert-base)',
         choices=[1, 2, 3],
-        default=2
+        default=1
+    )
+    parser.add_argument(
+        '--pos_model_level',
+        help='Choose which model to use for POS tagging (1: albert-tiny, 2: albert-base, 3: bert-base)',
+        choices=[1, 2, 3],
+        default=1
+    )
+    parser.add_argument(
+        '--ner_model_level',
+        help='Choose which model to use for NER (1: albert-tiny, 2: albert-base, 3: bert-base)',
+        choices=[1, 2, 3],
+        default=1
     )
     parser.add_argument(
         '--batch_size',
         help='Size of batch to be input into transformer models',
         type=int,
-        default=256
+        default=512
     )
     parser.add_argument(
         '--max_length',
@@ -64,40 +89,52 @@ def save_output(out_path: Path, obj: Union[List[str], List[NerToken]]) -> None:
     print(f'Object saved to {out_path.resolve()}')
 
 
-def run_pipeline(sents: List[str], out_path_base: Path, batch_size=64, max_length=509) -> Tuple[List[str], List[str], List[str]]:
-    word_sentence_list = ws(sents, batch_size=batch_size, max_length=max_length, use_delim=True)
-    save_output(out_path_base.joinpath('segmented.json'), word_sentence_list)
-
-    pos_sentence_list = pos(word_sentence_list, batch_size=batch_size, max_length=max_length, use_delim=True)
-    save_output(out_path_base.joinpath('pos.json'), pos_sentence_list)
+def run_pipeline(out_path_base: Path, sents: Optional[List[str]] = None, word_sentence_list: Optional[List[List[str]]] = None, batch_size=64, max_length=509) -> None:
+    if 'ws' in args.tasks:
+        if word_sentence_list:
+            print('Tokenized sentences already provided. Skipping tokenization.')
+        else:
+            print('Running tokenization...')
+            ws = CkipWordSegmenter(level=args.ws_model_level, device=args.device)
+            word_sentence_list = ws(sents, batch_size=batch_size, max_length=max_length, use_delim=True)
+            save_output(out_path_base.joinpath('segmented.json'), word_sentence_list)
+    
+    if 'pos' in args.tasks:
+        print('Running POS tagging...')
+        pos = CkipPosTagger(level=args.pos_model_level, device=args.device)
+        pos_sentence_list = pos(word_sentence_list, batch_size=batch_size, max_length=max_length, use_delim=True)
+        save_output(out_path_base.joinpath('pos.json'), pos_sentence_list)
 
     del word_sentence_list
     del pos_sentence_list
 
-    entity_sentence_list = ner(sents, batch_size=batch_size, max_length=max_length, use_delim=True)
-    save_output(out_path_base.joinpath('entity.json'), entity_sentence_list)
+    if 'ner' in args.tasks:
+        print('Running NER...')
+        ner = CkipNerChunker(level=args.ner_model_level, device=args.device)
+        entity_sentence_list = ner(sents, batch_size=batch_size, max_length=max_length, use_delim=True)
+        save_output(out_path_base.joinpath('entity.json'), entity_sentence_list)
     
-    return word_sentence_list, pos_sentence_list, entity_sentence_list
 
 
 if __name__ == '__main__':
     args = parse_args()
     args.output_path.mkdir(exist_ok=True)
 
-    ws = CkipWordSegmenter(level=args.model_level, device=args.device)
-    pos = CkipPosTagger(level=args.model_level, device=args.device)
-    ner = CkipNerChunker(level=args.model_level, device=args.device)
+    if args.tokenized_file:
+        with args.tokenized_file.open() as f:
+            tokens = json.load(f)
+        run_pipeline(word_sentence_list=tokens, batch_size=args.batch_size, max_length=args.max_length, out_path_base=args.output_path)
+    else:
+        with args.input_path.open() as f:
+            xml = bs(f, 'lxml')
 
-    with args.input_path.open() as f:
-        xml = bs(f, 'lxml')
+        texts = xml.findAll('text')
+        sent_list = [t.text.replace('\n', '') for t in texts]
+        if args.to_traditional:
+            converter = opencc.OpenCC('s2t.json')
+            sent_list = [converter.convert(t) for t in sent_list]
 
-    texts = xml.findAll('text')
-    sent_list = [t.text.replace('\n', '') for t in texts]
-    if args.to_traditional:
-        converter = opencc.OpenCC('s2t.json')
-        sent_list = [converter.convert(t) for t in sent_list]
+        del xml
+        del texts
 
-    del xml
-    del texts
-
-    w, p, e = run_pipeline(sent_list, batch_size=args.batch_size, max_length=args.max_length, out_path_base=args.output_path)
+        run_pipeline(sents=sent_list, batch_size=args.batch_size, max_length=args.max_length, out_path_base=args.output_path)
